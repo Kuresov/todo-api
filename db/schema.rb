b0cbba197d68db2474e21879f73be0f8d1309b2d
Alex Kirsopp <alex.kirsopp@gmail.com>
2015-08-29 15:36:11 -0400

    Basic HTTP user authentication. May switch to a password hash/salt variation later, and use Warden.

--- db/schema.rb
+++ db/schema.rb
@@ -34,10 +34,9 @@ ActiveRecord::Schema.define(version: 20150823173833) do
   create_table "users", force: :cascade do |t|
     t.string   "name"
     t.string   "email"
-    t.string   "password_hash"
-    t.string   "password_salt"
-    t.datetime "created_at",    null: false
-    t.datetime "updated_at",    null: false
+    t.string   "password"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
   end
 
 end
