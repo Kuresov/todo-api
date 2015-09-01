c973f3561ffe308dbc54c6c522dc035dc1ff361e
Alex Kirsopp <alex.kirsopp@gmail.com>
2015-09-01 19:05:25 -0400

    Added create by command line options, seem to have a problem with errors rendering the Rails error page rather than returning json

--- app/controllers/api/items_controller.rb
+++ app/controllers/api/items_controller.rb
@@ -8,8 +8,7 @@ class Api::ItemsController < ApiController
     if item.save
       render json: item
     else
-      #render json: { errors: item.errors.full_messages }, status: :unprocessable_entity
-      render json: item, status: :unprocessable_entity
+      render json: { errors: item.errors.full_messages }, status: :unprocessable_entity
     end
   end
 
