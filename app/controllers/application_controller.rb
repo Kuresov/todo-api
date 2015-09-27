d84709c09a7ba23dc477ea45e9461e429e5bb35d
Alex Kirsopp <alex.kirsopp@gmail.com>
2015-09-27 14:43:18 -0400

    Forgot to remove reference to Redis

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,5 +1,4 @@
 class ApplicationController < ActionController::Base
-  #include RedisStore
   include Authorization
   # Prevent CSRF attacks by raising an exception.
   # For APIs, you may want to use :null_session instead.
